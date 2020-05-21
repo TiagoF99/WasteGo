2cc7be5b298f0a88ee802c2e2a54579401b34feb
Tiago Ferreira <t.ferreiraibm.com@Tiagos-MacBook-Pro.local>
2020-05-21 16:30:20 -0400

    added initial graphql setup

--- app/server/src/app/data/mongo.go
+++ app/server/src/app/data/mongo.go
@@ -6,4 +6,5 @@ import (
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
+// export Client variable
 var Client, err = mongo.Connect(context.Background(), "Insert your MongoDB URI here!", nil)
--- app/server/src/app/main.go
+++ app/server/src/app/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"app/mutations"
+	"app/queries"
 	"log"
 	"net/http"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
+var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query:    queries.RootQuery,
+	Mutation: mutations.RootMutation,
+})
+
 func main() {
+	h := handler.New(&handler.Config{
+		Schema: &schema,
+		Pretty: true,
+	})
 
-	// Listens for any http request to localhost 3000
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World!")
-	}))
-
+	// open a new graphql endpoint
+	http.Handle("/graphql", disableCors(h))
 	log.Println("Now server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
+}
+
+func disableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
